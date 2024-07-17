65f2ef14e58633b2b7b814616d04e05d271bc609
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-07-17 11:48:36 +0530

    Create Students Api

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok("This is the list of students");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            return Ok("This is the student with id: " + id);
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent()
+        {
+            return Ok("Student added");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id)
+        {
+            return Ok("Student updated with id: " + id);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Student deleted with id: " + id);
+        }
+    }
+}
